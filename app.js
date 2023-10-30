94ba69cbda806be2d7d89e7a8b5c6dd0f95148fe
MohamadCode <m-munla2004@hotmail.com>
2023-10-30 23:05:15 +0300

    styling

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 let firstCard = 4
-let secondCard = 8
+let secondCard = 20
 let cardsSum = firstCard + secondCard
 
 
-if (cardsSum < 21) {
+if (cardsSum <= 21) {
     console.log("Do you want to draw a new card??")
 } else if (cardsSum === 21) {
     console.log("you've got a blackjack")
-} else (cardsSum > 21); {
+} else {
     console.log("You're out of the game!")
 }
