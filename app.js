ca7cc182e015224b2d1d2ccabad9e28a7e43598f
MohamadCode <m-munla2004@hotmail.com>
2023-10-30 23:14:48 +0300

    btn

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ let secondCard = 20
 let cardsSum = firstCard + secondCard
 
 
-if (cardsSum <= 21) {
-    console.log("Do you want to draw a new card??")
-} else if (cardsSum === 21) {
-    console.log("you've got a blackjack")
-} else {
-    console.log("You're out of the game!")
+
+
+
+function startGame() {
+    
+    if (cardsSum <= 21) {
+        console.log("Do you want to draw a new card??")
+    } else if (cardsSum === 21) {
+        console.log("you've got a blackjack")
+    } else {
+        console.log("You're out of the game!")
+    }
 }
+
