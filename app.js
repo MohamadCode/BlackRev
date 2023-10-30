c06bb3220a6448cec3067a733ac5310c4280d7c5
MohamadCode <m-munla2004@hotmail.com>
2023-10-30 22:40:03 +0300

    conditions

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ let firstCard = 4
 let secondCard = 8
 let cardsSum = firstCard + secondCard
 
-console.log(cardsSum)
+
+if (cardsSum < 21) {
+    console.log("Do you want to draw a new card??")
+} else if (cardsSum === 21) {
+    console.log("you've got a blackjack")
+} else (cardsSum > 21); {
+    console.log("You're out of the game!")
+}
