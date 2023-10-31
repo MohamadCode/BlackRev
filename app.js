aac5d53da8a286bcbfa8a5f1c1855e321b601f3b
MohamadCode <m-munla2004@hotmail.com>
2023-10-31 11:19:35 +0300

    login

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 let firstCard = 4
-let secondCard = 20
+let secondCard = 2
 let cardsSum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
-
+let sumEl = document.getElementById("sum-el")
 
 function startGame() {
-
+    sumEl.textContent += " " + cardsSum
+    
     if (cardsSum <= 21) {
         message = "Do you want to draw a new card??"
     } else if (cardsSum === 21) {
